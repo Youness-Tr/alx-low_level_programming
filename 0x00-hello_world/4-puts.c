40406260310d687739b0981b4064779afab35a40
Youness-Tr <younesstarwni2003@gmail.com>
2023-03-16 23:03:26 +0100

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
 
+/**
+ * main: entry
+ *
+ * puts use: print
+ *
+ * description: print with puts function
+ *
+ * return: aliwys 0 
+*/
 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
