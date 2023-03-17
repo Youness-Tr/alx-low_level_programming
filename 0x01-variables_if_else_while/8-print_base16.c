fc06339b554b88957c2e2626a5669b649885b2a0
Youness-Tr <younesstarwni2003@gmail.com>
2023-03-17 14:55:10 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry piont
+ * Description: 'print numbers of base 16'
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+	int a;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (a = 97; a <= 102; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
