d84fe48849da21a2b36face9b151c0b3d8b15b93
Youness-Tr <younesstarwni2003@gmail.com>
2023-03-21 11:43:51 -0700

    print_putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: 'program write _putchar'
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+learn functions nested loops
