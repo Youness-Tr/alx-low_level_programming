619eb16980fee6d8bc7051ca2d23a48102f8dbc9
Youness-Tr <younesstarwni2003@gmail.com>
2023-04-11 19:45:34 -0700

    mandatory tasks

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ *
+ * @grid: the grid
+ *
+ * owner: youness tr
+ * @height: height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a < height; a++)
+		free(grid[a]);
+	free(grid);
+}
