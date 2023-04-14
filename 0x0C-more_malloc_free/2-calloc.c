38261fdf98b196cd630c1b5187548be351212215
Youness-Tr <younesstarwni2003@gmail.com>
2023-04-13 18:58:59 -0700

    malloc tasks fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	a = malloc(nmemb * sizeof(int));
+	a = malloc(nmemb * size);
 
 	if (a == NULL)
 		return (NULL);
