9b0dfd0a4ba76764709c0fff50bffc73dfb52b52
Youness-Tr <younesstarwni2003@gmail.com>
2023-05-02 05:48:51 -0700

    add a task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 
-void first(void) __attribute__ ((constructor));
+void _const(void) __attribute__ ((constructor));
 
 /**
  * const - prints a sentence before the main
  * Return: void
  */
-void const(void)
+void _const(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
