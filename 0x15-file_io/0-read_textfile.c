fdb3da99ff611c2733613909e5fe3f469573648e
Youness-Tr <younesstarwni2003@gmail.com>
2023-05-08 16:57:53 -0700

    trying fo fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,28 +9,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fdest;
-	ssize_t read, writ;
-	char *buffer;
+	int fd;
+	ssize_t r, w;
+	char *buf;
 
 	if (!filename)
 		return (0);
 
-	fdest = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-	if (fdest == -1)
+	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * (letters));
-	if (!buffer)
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
 
-	read = read(fdest, buffer, letters);
-	writ = write(STDOUT_FILENO, buffer, read);
+	r = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
 
-	close(fdest);
+	close(fd);
 
-	free(buffer);
+	free(buf);
 
-	return (writ);
+	return (w);
 }
